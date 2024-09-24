56c667e5766f44d3a297c568b5bceef46eadf511
Santhosh Shanmugam <santhoshs192005@gmail.com>
2024-09-24 21:46:03 +0530

    Value tests

--- JobFinder/src/Components/ValueDiv/Value.jsx
+++ JobFinder/src/Components/ValueDiv/Value.jsx
@@ -1,14 +1,52 @@
 import React from 'react'
-
+import { CiApple } from "react-icons/ci";
 const Value = () => {
   return (
-    <div className=''>
-        <strong><div className='mt-20 text-[25px] mb-15'>The value that holds us true and to account</div></strong>
-        <div className='flex gap-6 p-[10px] w-[100%]'>
-             <div className=''>
-                
-             </div>
+    <div  className="container">
+    <h1 className='text-[25px] mt-20 mb-20'>The value that holds us true and to account</h1>
+    <div className='grid gap-[10rem] grid-cols-3 items-center border-solid'>
+        <div className='rounded-[10px] hover:bg-[#fcfae3] p-[1.5rem]'>
+            <div className='flex items-center gap-3'>
+                <div className='p-[4px] rounded-[.8rem] bg-[#f3f2ae3] h-[40px] w-[40px] flex items-center justify-center'>
+                    <CiApple className='text-[25px]'/>
+                </div>
+                <span className='font-semibold tect-textColor text-[18px]'>
+                    Simplicity
+                </span>
+            </div>
+            <p className='text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold'>
+                Things beinf made beautiful simple are at the heart of everything
+            </p>
         </div>
+
+        <div className='rounded-[10px] hover:bg-[#fcfae3] p-[1.5rem]'>
+            <div className='flex items-center gap-3'>
+                <div className='p-[4px] rounded-[.8rem] bg-[#f3f2ae3] h-[40px] w-[40px] flex items-center justify-center'>
+                    <CiApple className='text-[25px]'/>
+                </div>
+                <span className='font-semibold tect-textColor text-[18px]'>
+                    Simplicity
+                </span>
+            </div>
+            <p className='text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold'>
+                Things beinf made beautiful simple are at the heart of everything
+            </p>
+        </div>
+
+        <div className='rounded-[10px] hover:bg-[#fcfae3] p-[1.5rem]'>
+            <div className='flex items-center gap-3'>
+                <div className='p-[4px] rounded-[.8rem] bg-[#f3f2ae3] h-[40px] w-[40px] flex items-center justify-center'>
+                    <CiApple className='text-[25px]'/>
+                </div>
+                <span className='font-semibold tect-textColor text-[18px]'>
+                    Simplicity
+                </span>
+            </div>
+            <p className='text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold'>
+                Things beinf made beautiful simple are at the heart of everything
+            </p>
+        </div>
+    </div>
     </div>
   )
 }
