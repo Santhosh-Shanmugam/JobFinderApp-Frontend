8dbd6aa25ee0ca1874c640454598f58a9c5c9cdd
Santhosh Shanmugam <santhoshs192005@gmail.com>
2024-09-24 18:17:26 +0530

    navbar

--- JobFinder/src/App.jsx
+++ JobFinder/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Navbar from './Components/NavbarDiv/Navbar'
+import Search from './Components/SearchDiv/Search'
+import Job from './Components/JobDiv/Job'
+import Value from './Components/ValueDiv/Value'
+import Footer from './Components/FooterDiv/Footer'
+import { BrowserRouter, Route, Router } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar/>
+      <Search/>
+      <Job/>
+      <Value/>
+      <Footer/>
     </div>
   )
 }
--- JobFinder/src/Components/NavbarDiv/Navbar.jsx
+++ JobFinder/src/Components/NavbarDiv/Navbar.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='flex justify-between items-center p-[3rem]'>
+        <div className='logo'>
+              <h1 className='text-[25px] text-blueColor'><strong>Job</strong><bold>Finder</bold></h1>
+        </div>
+        <div className='flex gap-8 text-[20px] p-3 m-3'>
+            <li className='text-black hover:text-blueColor'>Jobs</li>
+            <li className='text-black hover:text-blueColor'>About</li>
+            <li className='text-black hover:text-blueColor'>Blog</li>
+            <li className='text-black hover:text-blueColor'>Companies</li>
+            <li className='text-black hover:text-blueColor'>Contact</li>
+        </div>
+        <div className='gap-8 flex'><button className='p-3 text-white bg-blueColor rounded-lg'>Login</button>
+            <button className='p-3 text-white bg-blueColor rounded-lg'>SignUp</button>
+        </div>
+            
     </div>
   )
 }
